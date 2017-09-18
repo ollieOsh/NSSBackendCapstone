236ca6251ead7bf17b8b106e01e1f08ec88b5761
Segun Osinusi <sosinusi@gmail.com>
2017-09-18 11:25:23 -0500

    initial setup

--- SoundClout/Controllers/HomeController.cs
+++ SoundClout/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SoundClout.Models;
 
 namespace SoundClout.Controllers
 {
@@ -30,6 +31,35 @@ namespace SoundClout.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public int Reduce (int date)
+        {
+            int finalNum = 0;
+
+            while(date > 9 && date != 11 && date != 22 && date != 33)
+            {
+                var splitDate = date.ToString().Split();
+
+                for( int i = 0; i < splitDate.Length; i++)
+                {
+                    finalNum += Int32.Parse(splitDate[i]);
+                    date = finalNum;
+                }
+            }
+
+            return finalNum;
+
+        }
+
+        public int MakeDestiny()
+        {
+            DateTime birthDate = DateTime.Now;
+            int day = Reduce(birthDate.Day) + 1;
+            int month = Reduce(birthDate.Month);
+            int year = Reduce(birthDate.Year);
+
+            return Reduce(day + month + year);
         }
     }
 }
